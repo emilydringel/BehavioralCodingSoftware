0e6efaa1d0eefa915265dbdb9bc0e21e197eb605
emilydringel <edr2124@barnard.edu>
2022-02-20 11:47:44 -0500

    Fill to match to do app

--- main.js
+++ main.js
@@ -1,43 +1,69 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+'use strict'
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+const path = require('path')
+const { app, ipcMain } = require('electron')
+
+const Window = require('./Window')
+const DataStore = require('./DataStore')
+
+require('electron-reload')(__dirname)
+
+// create a new todo store name "Todos Main"
+const todosData = new DataStore({ name: 'Todos Main' })
+
+function main () {
+  // todo list window
+  let mainWindow = new Window({
+    file: path.join('renderer', 'index.html')
+  })
+
+  // add todo window
+  let addTodoWin
+
+  // TODO: put these events into their own file
+
+  // initialize with todos
+  mainWindow.once('show', () => {
+    mainWindow.webContents.send('todos', todosData.todos)
+  })
+
+  // create add todo window
+  ipcMain.on('add-todo-window', () => {
+    // if addTodoWin does not already exist
+    if (!addTodoWin) {
+      // create a new add todo window
+      addTodoWin = new Window({
+        file: path.join('renderer', 'add.html'),
+        width: 400,
+        height: 400,
+        // close with the main window
+        parent: mainWindow
+      })
+
+      // cleanup
+      addTodoWin.on('closed', () => {
+        addTodoWin = null
+      })
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // add-todo from add todo window
+  ipcMain.on('add-todo', (event, todo) => {
+    const updatedTodos = todosData.addTodo(todo).todos
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    mainWindow.send('todos', updatedTodos)
+  })
+
+  // delete-todo from todo list window
+  ipcMain.on('delete-todo', (event, todo) => {
+    const updatedTodos = todosData.deleteTodo(todo).todos
+
+    mainWindow.send('todos', updatedTodos)
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready', main)
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
---
+++ renderer/renderer.js
@@ -0,0 +1,41 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+'use strict'
+
+const { ipcRenderer } = require('electron')
+
+// delete todo by its text value ( used below in event listener)
+const deleteTodo = (e) => {
+  ipcRenderer.send('delete-todo', e.target.textContent)
+}
+
+// create add todo window button
+document.getElementById('createTodoBtn').addEventListener('click', () => {
+  ipcRenderer.send('add-todo-window')
+})
+
+// on receive todos
+ipcRenderer.on('todos', (event, todos) => {
+  // get the todoList ul
+  const todoList = document.getElementById('todoList')
+
+  // create html string
+  const todoItems = todos.reduce((html, todo) => {
+    html += `<li class="todo-item">${todo}</li>`
+
+    return html
+  }, '')
+
+  // set list html to the todo items
+  todoList.innerHTML = todoItems
+
+  // add click handlers to delete the clicked todo
+  todoList.querySelectorAll('.todo-item').forEach(item => {
+    item.addEventListener('click', deleteTodo)
+  })
+})
